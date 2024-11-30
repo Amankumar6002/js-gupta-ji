db878821c997e8c51a96ae97fddf0c9ce92e91fc
Aman kumar <vikashkumargupta368@gmail.com>
2024-11-30 11:22:46 +0000

    DATA TYPE

---
+++ 01_basic/data_type.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all js code as newer version
+console.log(3+6)
+ 
+let name = "Aman"
+let age = 18
+let isLoggedIn = false
+//number =>2 to power 53
+console.log(typeof null); // null is an object
+console.log(typeof "Aman");
