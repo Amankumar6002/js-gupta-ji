8428ad02b513350dae86ab24765d6598520a259b
Aman kumar <vikashkumargupta368@gmail.com>
2024-11-30 10:59:21 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14566343
+let accountEmail = "amankr234@google.com"
+var accountPassword = "12345"
+accountCity = "Aurangabad"
+let accountStstus;
+//accountId = 3
+accountEmail = "aj@aman.com"
+accountPassword = "2325431"
+accountCity = "Bihar"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountStstus])
