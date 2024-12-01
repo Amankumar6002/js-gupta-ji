cad558de5c7642670fcaa6c0b6c18ccd9650e8b6
Aman kumar <vikashkumargupta368@gmail.com>
2024-12-01 11:53:51 +0000

    consevationOperation

---
+++ 01_basic/consersionOperation.js
@@ -0,0 +1,34 @@
+let score = 33
+
+
+console.log(typeof score);
+let sc = "23"
+console.log(typeof sc);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn  = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// ***************** Operations ************
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " Aman"
+str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+console.log(+true);
+console.log(+"");
+
+
+
+
+
