ea98064ebf58d9136b714237ab1deae637bfbaa0
Aman kumar <vikashkumargupta368@gmail.com>
2024-12-03 11:43:13 +0000

    arrow_function

---
+++ 03_basic/arrow_functions.js
@@ -0,0 +1,41 @@
+// const user = { 
+//     username: " aman",
+//     price: 999,
+//     welcomeMessage:function(){
+// console.log(`${this.username}, welcome to website`);
+// console.log(this);
+
+//     }
+// }
+// user.welcomeMessage()
+// user.username = "gupta"
+// user.welcomeMessage()
+
+
+// function aj(){
+//     let username = "aman"
+//     console.log(this);
+    
+// }
+
+// const aj =() =>{
+//     let username = "aman"
+//     console.log(this);
+    
+// }
+// aj()
+
+
+// console.log();
+
+
+
+// const addTwo = (n1, n2) =>{
+//     return n1 + n2
+// }
+// console.log(addTwo(3, 4));
+
+
+const addTwo = (n1, n2) => (n1 + n2)
+
+console.log(addTwo(3, 4))
